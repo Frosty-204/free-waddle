1e59ad87564b658696dc390192170d64ab2b4005
Frosty-204 <82284925+Frosty-204@users.noreply.github.com>
2024-06-07 00:38:07 +0300

    Update viewsController.js

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -40,6 +40,12 @@ exports.getTour = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getSignUp = (req, res) => {
+  res.status(200).render('signup', {
+    title: 'Sign Up'
+  });
+};
+
 exports.getLoginForm = (req, res) => {
   res.status(200).render('login', { title: 'Log into your account' });
 };
